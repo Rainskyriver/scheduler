e594ff658da60f4b71e16c9fcaf0082c3757c412
Rainskyriver <lianyelich14@gmail.com>
2020-02-17 12:32:23 -0800

    Day 1 Start

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
+import DayList from "components/DayListItem"
 
 ReactDOM.render(<Application />, document.getElementById("root"));
