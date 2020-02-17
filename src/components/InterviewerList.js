9f6f5bef0f510d39cbc291bf485440e4e1a62481
Rainskyriver <lianyelich14@gmail.com>
2020-02-17 15:04:07 -0800

    Add InterviewerList and InterviewerListItem

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
+    );
+  });
+  return interviewers;
+}
